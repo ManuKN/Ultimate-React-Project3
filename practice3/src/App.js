0af3f12f1b4f18a5db81528a059be3bb206437ed
Manu K N <201910101325@presidencyuniversity.in>
2023-12-11 15:19:36 +0530

    Changes Added Successfully

--- practice3/src/App.js
+++ practice3/src/App.js
@@ -1,17 +1,17 @@
 import { useState } from 'react';
 import './App.css';
 
-const Itemslist = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "charger", quantity: 1, packed: true },
-  { id: 4, description: "Brush", quantity: 2, packed: false },
-];
+// const Itemslist = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "charger", quantity: 1, packed: true },
+//   { id: 4, description: "Brush", quantity: 2, packed: false },
+// ];
 function App() {
   const[items , setitems] = useState([])
   
   function handleAddItems(item){
-    console.log(item)
+    console.log({item})
       setitems((items) => [...items,item]);
   }
 
@@ -22,12 +22,19 @@ function App() {
   function handleToggleItems(id){
     setitems((items)=>items.map((item)=>item.id === id ? {...item , packed:!item.packed} : item));
   }
+
+  function handleClearList(){
+    // alert("Are you sure u want to clear all items?🤔")?
+    const Confirmed = window.confirm("Are u sure u Want to Clear all items ?🤔")
+    if(Confirmed)
+    setitems([])
+  }
   return (
     <div className="app">
       <Logo/>
       <Form  onAdditems={handleAddItems}/>
-      <PackingList  Items={items} onDeleteItem={handleDelete} onToogleItem={handleToggleItems}/>
-      <Stats/>
+      <PackingList  Items={items} onDeleteItem={handleDelete} onToogleItem={handleToggleItems} onClearItem={handleClearList}/>
+      <Stats items={items}/>
     </div>
   );
 }
@@ -43,8 +50,8 @@ function Form({onAdditems}){
   function eventhandler(e){
    e.preventDefault();
    if(!description)
-   return alert("Please Enter Item")
-  const newItem = {description,quality, placed:false , id:Date.now() }
+   return alert("Please Enter Item 🙌")
+  const newItem = {description,quality, packed:false , id:Date.now() }
 
   onAdditems(newItem);
   setDesc("")
@@ -62,12 +69,31 @@ return(
 )
 }
 
-function PackingList({Items , onDeleteItem , onToogleItem}){
+function PackingList({Items , onDeleteItem , onToogleItem , onClearItem}){
+  const[sortBy,setSortBy] = useState("input") ;
+  let sortedItems;
+  console.log({sortedItems})
+  if(sortBy==="input")
+     sortedItems= Items;
+
+     if(sortBy==="description")
+     sortedItems= Items.slice().sort((a ,b)=> a.description.localeCompare(b.description.localeCompare))
+
+     if(sortBy==="packed")
+     sortedItems=Items.sort((a ,b)=>Number(a.packed)- Number(b.packed));
 return (
 <div className='list'>
   <ul>
-{Items.map((item)=>(<Item items={item} onDeleteItem={onDeleteItem} onToogleItem={onToogleItem} key={item.id}/>))}
+{sortedItems.map((item)=>(<Item items={item} onDeleteItem={onDeleteItem} onToogleItem={onToogleItem} key={item.id}/>))}
 </ul>
+<div className='actions'>
+ <select value={sortBy} onChange={(e)=>setSortBy(e.target.value)}>
+  <option value='input'>Sort Items by input Order </option>
+  <option value='description'>Sort Items by Description Order </option>
+  <option value='packed'>Sort Items by Packed Order </option>
+ </select>
+ <button onClick={onClearItem}>Clear List</button>
+</div>
 </div>
 )
 }
@@ -84,9 +110,17 @@ function Item({items , onDeleteItem, onToogleItem}){
           </li>
         )
 }
-function Stats(){
+function Stats({items}){
+if(!items.length)
+return <p><em className='footer'>Start Adding Items to your Packing List🚀😊</em></p>
+else{const NumItems = items.length;
+  const Numpacked = items.filter((item)=>item.packed).length
+  const Percentage = Math.round(Numpacked/NumItems*100)
    return <footer className='stats'>
-    You have X 💼items on your list ,and you already packed X items😍 (X%)
-   </footer>
+    <em>
+      {Percentage === 100 ? "You Got Everthing! Ready to Go ✈️🌍":
+    `You have ${NumItems} 💼items on your list ,and you already packed ${Numpacked} items😍 (${Percentage}%)💪🤟`}
+    </em>
+   </footer>}
 }
 export default App;
